d774d1c52485399400c3eb31aec5378c7d948209
jihoon <wlgns2007@gmail.com>
2026-05-13 01:08:48 +0900

    made professor DB schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProfessorService } from './professor/professor.service';
+import { ProfessorController } from './professor/professor.controller';
+import { ProfessorModule } from './professor/professor.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProfessorModule],
+  controllers: [AppController, ProfessorController],
+  providers: [AppService, ProfessorService],
 })
 export class AppModule {}
